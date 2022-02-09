5ca1f853cdea5848850f05e60ff2da3ebfbbfbe0
Shaun Ostoic <ostoic@uwindsor.ca>
2022-02-08 23:02:00 -0500

    Initial commit

---
+++ extension/hook-index.js
@@ -0,0 +1,44 @@
+console.log("Tabster loaded");
+chrome.webRequest.onHeadersReceived.addListener(
+    function(details) {
+        // if (!details.responseHeaders) {
+        //     return;
+        // }
+
+        console.log(details);
+        if(details.url.includes('.js'))
+        {
+            for (var i = 0; i < details.responseHeaders.length; ++i) {
+              if (details.responseHeaders[i].name.toLowerCase() === 'content-type') {
+                const name = details.responseHeaders[i].name;
+                details.responseHeaders[i] = {name: name, value: 'application/javascript'};
+                console.log("Modified Content-Type:", details)
+                break;
+              }
+            }
+
+            return {responseHeaders: details.responseHeaders};
+        }
+    },
+    {urls: [ "https://raw.githubusercontent.com/Ostoic/random-scripts/main/*.js" ] },
+    ["blocking", "responseHeaders"]
+);
+
+// const github_script_regex = /https?[:][/]{2}raw.githubusercontent.com[/]Ostoic[/]random-scripts[/]main[/]([a-zA-Z0-9.]+.js)/ig;
+// const songsterr_script_regex = /https?[:][/]{2}www.songsterr.com[/]static[/]([a-zA-Z0-9.]+.js)/ig;
+
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+        // const result = github_script_regex.exec(details.url);
+        // if (result) {
+        //     console.log("Fixing github redirect script to github script");
+        //     return {redirectUrl: "https://www.songsterr.com/static/" + result[1]};
+        // }
+        if (details.url == "https://www.songsterr.com/static/index.52ddb71b.js")
+        {
+            return {redirectUrl: "https://raw.githubusercontent.com/Ostoic/random-scripts/main/index.52ddb71b.js" };
+        }
+    },
+    {urls: ["https://www.songsterr.com/static/index.52ddb71b.js"]},
+    ["blocking"]
+);
