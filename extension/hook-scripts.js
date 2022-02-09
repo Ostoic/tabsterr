66c5e1db4e57eeb946fad6b849d7d0cd3cc9a473
Shaun Ostoic <ostoic@uwindsor.ca>
2022-02-09 16:28:17 -0500

    Redirect any version of index script

--- extension/hook-scripts.js
+++ extension/hook-scripts.js
@@ -1,10 +1,6 @@
 console.log("Tabster loaded");
 chrome.webRequest.onHeadersReceived.addListener(
     function(details) {
-        // if (!details.responseHeaders) {
-        //     return;
-        // }
-
         console.log(details);
         if(details.url.includes('.js'))
         {
@@ -24,21 +20,17 @@ chrome.webRequest.onHeadersReceived.addListener(
     ["blocking", "responseHeaders"]
 );
 
-// const github_script_regex = /https?[:][/]{2}raw.githubusercontent.com[/]Ostoic[/]random-scripts[/]main[/]([a-zA-Z0-9.]+.js)/ig;
-// const songsterr_script_regex = /https?[:][/]{2}www.songsterr.com[/]static[/]([a-zA-Z0-9.]+.js)/ig;
-
+const songsterr_index_js_regex = /https?[:][/]{2}www.songsterr.com[/]static[/]index.([a-zA-Z0-9.]+).js/ig;
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
-        // const result = songsterr_script_regex.exec(details.url);
-        // if (result) {
-        //     console.log("Redirecting script");
-        //     return {redirectUrl: "https://www.songsterr.com/static/" + result[1]};
-        // }
-        if (details.url == "https://www.songsterr.com/static/index.52ddb71b.js")
-        {
-            return {redirectUrl: "https://raw.githubusercontent.com/Ostoic/tabsterr/main/scripts/js/index.52ddb71b.js" };
+        const result = songsterr_index_js_regex.exec(details.url);
+        if (result) {
+            const index_js_filename = "index." + result[1] + ".js";
+            console.log("Redirecting " + index_js_filename + " script");
+            let github_index_script = "https://raw.githubusercontent.com/Ostoic/tabsterr/main/scripts/js/" + index_js_filename;
+            return {redirectUrl: github_index_script};
         }
     },
-    {urls: ["https://www.songsterr.com/static/index.52ddb71b.js"]},
+    {urls: ["https://www.songsterr.com/static/index.*.js"]},
     ["blocking"]
 );
