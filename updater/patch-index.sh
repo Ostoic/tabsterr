ffe87c2201988acc30b7b1e1550ff59dbd4eab2a
Shaun Ostoic <ostoic@uwindsor.ca>
2022-02-21 04:37:25 -0500

    Index update

--- updater/patch-index.sh
+++ updater/patch-index.sh
@@ -5,30 +5,38 @@ if [ $# -ne 1 ]; then
   exit -1
 fi
 
+function make-patch() {
+  num_bytes_before_patch1=$(wc -c $3 | cut -d ' ' -f1)
+  if [ $# == 4 ]; then
+    sed -i -E "s|$1|$2|g" $3
+  else
+    sed -i "s|$1|$2|g" $3
+  fi
+
+  new_size=$(wc -c $3 | cut -d ' ' -f1)
+
+  if [ $num_bytes_before_patch1 == $new_size ]; then
+    echo 'Patch was not applied'
+    exit -2
+  fi
+}
+
 FIND1='";var w={app:"Cdy1hg"'
-FIND2='await Promise.resolve(),b(i(I.Provider,{value:t}'
 PATCH1='";document.getElementById("app").classList.remove("Cdy160");var w={app:"Cdy1hg"'
+
+FIND2='await Promise.resolve(),b(i(I.Provider,{value:t}'
 PATCH2='(t.state.user={profile:null,hasPlus:true,hasSubscription:true,isAdmin:false,isModerator:false,isLoggedIn:false}, await Promise.resolve()),b(i(I.Provider,{value:t}'
 
-# Make first patch
-echo "Adding css patch..."
-num_bytes_before_patch1=$(wc -c $1 | cut -d ' ' -f1)
-sed -i "s|$FIND1|$PATCH1|g" $1
-new_size=$(wc -c $1 | cut -d ' ' -f1)
+FIND3='\./([a-zA-Z0-9.]+)'
+PATCH3='https://www.songsterr.com/static/\1'
 
-if [ $num_bytes_before_patch1 == $new_size ]; then
-  echo 'Patch 1 was not applied'
-  exit -2
-fi
+echo "Adding css patch..."
+make-patch "$FIND1" "$PATCH1" $1 
 
 echo "Adding js patch..."
-num_bytes_before_patch1=$(wc -c $1 | cut -d ' ' -f1)
-sed -i "s|$FIND2|$PATCH2|g" $1
-new_size=$(wc -c $1 | cut -d ' ' -f1)
+make-patch "$FIND2" "$PATCH2" $1 
 
-if [ $num_bytes_before_patch1 == $new_size ]; then
-  echo 'Patch 2 was not applied'
-  exit -3
-fi
+echo "Adding scripts url patch..."
+make-patch "$FIND3" "$PATCH3" $1 exregex
 
 echo "Done!"
