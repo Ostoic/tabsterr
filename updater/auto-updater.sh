b5c1c0e854a6da24a0ce8609c1b65576e05a5847
Shaun Ostoic <ostoic@uwindsor.ca>
2022-04-13 18:53:35 -0400

    Update for latest version, updater scripts marginally improved

--- updater/auto-updater.sh
+++ updater/auto-updater.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+INDEX_ERR_ALREADY_PATCHED=1
+INDEX_ERR_WGET=2
 
 update-repo() {
   mv index.*.js ../scripts/js/
@@ -8,25 +10,28 @@ update-repo() {
   echo 'New patch committed'
 }
 
-check-latest-index() {
-  echo 'Checking for new index...'
-  if ! ./get-latest-index.sh; then
-    return 1
-  fi
-}
-
 if [ ! -f ./get-latest-index.sh ] || [ ! -f ./patch-index.sh ]; then
   echo "Missing patch scripts. Cd to the updater directory from the project root"
   exit -1
 fi
 
-check-latest-index
+echo 'Checking for new index...'
+./get-latest-index.sh
+index=$?
+
 while true; do
-  echo 'Patching index...'
-  if ./patch-index.sh index.*.js; then
-    update-repo
+  if [ $index = $INDEX_ERR_WGET ]; then
+    echo "Error: wget 404. This means the website structure must have changed in some way"
+    exit -1
+  elif [ $index -ne $INDEX_ERR_ALREADY_PATCHED ]; then
+    echo 'Patching index...'
+    if ./patch-index.sh index.*.js 2>/dev/null; then
+      update-repo
+    fi
   fi
   
   sleep 3600
-  check-latest-index
+  echo 'Checking for new index...'
+  ./get-latest-index.sh
+  index=$?
 done
--- updater/get-latest-index.sh
+++ updater/get-latest-index.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+INDEX_ERR_ALREADY_PATCHED=1
+INDEX_ERR_WGET=2
+
 USER_AGENT='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36'
 INDEX_URL="https://www.songsterr.com/a/wsa/metallica-fade-to-black-tab-s20"
 NEW_INDEX=$(curl -A "$USER_AGENT" -s $INDEX_URL | grep -Eo '(index\.[a-z0-9]+\.js)')
@@ -6,8 +10,11 @@ echo $NEW_INDEX
 
 if [ -f ../scripts/js/$NEW_INDEX ]; then
   echo 'Already patched'
-  exit -1
+  exit $INDEX_ERR_ALREADY_PATCHED
 fi
 
 SCRIPTS_URL="https://www.songsterr.com/static/latest"
-wget "$SCRIPTS_URL/$NEW_INDEX"
+if ! wget -q "$SCRIPTS_URL/$NEW_INDEX" ; then
+  echo 'wget 404 error'
+  exit $INDEX_ERR_WGET
+fi
